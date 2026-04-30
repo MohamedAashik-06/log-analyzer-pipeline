531389e3b185d9647f7a57a2c3c2c95cb94920a0
MohamedAashik-06 <mohamedaashik0605@gmail.com>
2026-04-30 16:21:52 +0530

    analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -1,1 +1,26 @@
+log_file = "logs.txt"
+report_file = "report.txt"
 
+info = 0
+error = 0
+warning = 0
+
+# Read log file
+with open(log_file, "r") as file:
+    for line in file:
+        if "INFO" in line:
+            info += 1
+        elif "ERROR" in line:
+            error += 1
+        elif "WARNING" in line:
+            warning += 1
+
+# Write report
+with open(report_file, "w") as file:
+    file.write("Log Summary Report\n")
+    file.write("-------------------\n")
+    file.write(f"INFO: {info}\n")
+    file.write(f"ERROR: {error}\n")
+    file.write(f"WARNING: {warning}\n")
+
+print("Report generated successfully!")
